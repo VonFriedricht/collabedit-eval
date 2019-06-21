c38685e0d457515d413febe0be07b7b2ee8934a0
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-21 12:26:06 +0200

    Create Loop.js

---
+++ Loop.js
@@ -0,0 +1,33 @@
+class Loop {
+  constructor(name, tickspeed) {
+    this.name = name
+    this.interval = false
+    this.timer = 1000 / tickspeed
+    this.funct = () => {}
+  }
+  isset() {
+    return eval(`typeof ${this.name} != "undefined"`)
+  }
+  setTps(tps) {
+    this.timer = 1000 / tps
+  }
+  start() {
+    if (this.interval === false) {
+      this.interval = setInterval(() => this.funct(), this.timer)
+    }
+  }
+  end() {
+    if (this.interval !== false) {
+      clearInterval(this.interval)
+      this.interval = false
+    }
+  }
+  update() {
+    if (this.isset()) {
+      this.funct = eval(this.name)
+      this.start()
+    } else {
+      this.end()
+    }
+  }
+}
