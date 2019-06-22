9f95b53bfd2f05cfd9bd774dd9a3d463ff1b78b7
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-22 13:38:08 +0200

    cleanup and commenting

--- collabedit-eval.user.js
+++ collabedit-eval.user.js
@@ -9,11 +9,13 @@
 // @updateURL    https://github.com/VonFriedricht/collabedit-eval/raw/master/collabedit-eval.user.js
 // ==/UserScript==
 
+
+// Scoping
 var Loop
 var loops
-
 var lastCode
 
+// To import other files inside repo
 async function require(path) {
   let basePath = `https://raw.githubusercontent.com/VonFriedricht/collabedit-eval/master/`
   let response = await fetch(basePath + path)
@@ -21,17 +23,21 @@ async function require(path) {
   return eval(responseText)
 }
 
+// First Script at Startup
 async function init() {
   Loop = await require("Loop.js")
-  // chat = await require("chat.js")
-  // createCanvas = await require("createCanvas.js")
+  chat = await require("chat.js")
+  createCanvas = await require("createCanvas.js")
   loops = [
     new Loop("loop", 30), 
     new Loop("slowLoop", 10), 
     new Loop("clock", 1)
   ]
+  
+  chat.log("init passed!")
 }
 
+// Main-Loop after init
 function exec() {
   let frame = document.getElementById("frame_the_input")
   if (!frame) return false
@@ -49,29 +55,5 @@ function exec() {
   }
 }
 
-chat = {
-  log: message => {
-    var div = document.createElement("div")
-    div.className = "chtmsg"
-    div.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${message}</span>`
-    document.getElementById("message_div").appendChild(div)
-  }
-}
-
-createCanvas = (x, y) => {
-  let canvas = document.getElementById("canvas")
-  if (!canvas) {
-    canvas = document.createElement("canvas")
-    canvas.id = "canvas"
-    canvas.style.border = "1px dotted gray"
-    canvas.style.position = "absolute"
-    canvas.style.right = "4em"
-    canvas.style.top = "9em"
-    document.getElementById("main").prepend(canvas)
-  }
-  canvas.width = x
-  canvas.height = y
-  return canvas
-}
-
+// ↑ wake-up
 init().then(setInterval(exec, 1000))
