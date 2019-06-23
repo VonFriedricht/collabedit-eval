72d102a2f509e020a512e626b9cbf5c337ecf7b3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-23 16:09:34 +0200

    added JSON.stringify on message

--- chat.js
+++ chat.js
@@ -4,7 +4,7 @@ chat = {
     var msg = document.createElement("div")
     
     msg.className = "chtmsg"
-    msg.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${message}</span>`
+    msg.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${JSON.stringify(message)}</span>`
     
     chatDiv.appendChild(msg)
     chatDiv.scrollTop = chatDiv.scrollHeight
