bf28163a17aa3f31dfc3d4d466417c328e628858
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-24 12:31:47 +0200

    increment version

--- collabedit-eval.user.js
+++ collabedit-eval.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         collabedit-eval
 // @namespace    https://github.com/VonFriedricht/collabedit-eval
-// @version      0.2.1
+// @version      0.2.2
 // @description  evals the javascript in collabedit.com
 // @author       VonFriedricht
 // @match        http://collabedit.com/*
