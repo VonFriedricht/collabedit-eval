1afe7a5ad7561aec6027e29f770075f741b2eeba
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-24 10:57:39 +0200

    Create without_userscript.js

---
+++ without_userscript.js
@@ -0,0 +1,5 @@
+// To import this repo without userscript
+fetch(`https://raw.githubusercontent.com/VonFriedricht/collabedit-eval/master/collabedit-eval.user.js`)
+  .then(r => r.text)
+  .then(r => eval(r))
+  
