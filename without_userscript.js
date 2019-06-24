6e28dd76dfd7881c1ba43aa596f48d61b4fd0501
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-24 11:12:43 +0200

    fixed text fetch

--- without_userscript.js
+++ without_userscript.js
@@ -1,5 +1,4 @@
 // To import this repo without userscript
 fetch(`https://raw.githubusercontent.com/VonFriedricht/collabedit-eval/master/collabedit-eval.user.js`)
-  .then(r => r.text)
+  .then(r => r.text())
   .then(r => eval(r))
-  
