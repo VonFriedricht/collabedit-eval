0d6ca57b67f09c107b6a81d5dada35d67be2cc78
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-26 00:42:43 +0200

    added mouse and keyinput to canvas

--- createCanvas.js
+++ createCanvas.js
@@ -1,4 +1,4 @@
-createCanvas = (x, y) => {
+createCanvas = (x, y, input = false) => {
   let canvas = document.getElementById('canvas')
   if (!canvas) {
     canvas = document.createElement('canvas')
@@ -7,6 +7,13 @@ createCanvas = (x, y) => {
     canvas.style.position = 'absolute'
     canvas.style.right = '4em'
     canvas.style.top = '9em'
+    if(input){
+      canvas.onmousedown = e => {mouse.pressed = true}
+      canvas.onmouseup = e => {mouse.pressed = false}
+      canvas.onmousemove = e => {mouse.x = e.layerX; mouse.y = e.layerY}
+      document.onkeydown = e => {console.log(1, e)}
+      document.onkeyup = e => {console.log(0, e)}
+    }
     document.getElementById('main').prepend(canvas)
   }
   canvas.width = x
