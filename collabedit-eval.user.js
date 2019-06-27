d3ce8c7555f164c6bf42468f14a0f211ee5f8afe
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-27 10:38:02 +0200

    change translation to sinlgequote

--- collabedit-eval.user.js
+++ collabedit-eval.user.js
@@ -53,7 +53,7 @@ function exec() {
   code = code
     .replace(/for ([a-zA-Z]*?) in range\(([^\s]*?)\)/g, 'for(let $1 = 0; $1 < $2; $1++)')
     .replace(/for ([a-zA-Z]*?) in range\(([^\s]*?),([^\s]*?)\)/g, 'for(let $1 = $2; $1 <= $3; $1++)')
-    .replace(/([^\s]*?).clean\(\)/g,`if($1.canvas){$1.clearRect(0,0,$1.canvas.width,$1.canvas.height)}`)
+    .replace(/([^\s]*?).clean\(\)/g, `if($1.canvas){$1.clearRect(0,0,$1.canvas.width,$1.canvas.height)}`)
   let evalreturn = eval(code)
   console.log(evalreturn)
   for (let l of loops) {
