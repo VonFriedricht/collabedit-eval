fc3f46dc15b1c2450857c45ba44663a2ad1e002e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-06-29 09:48:40 +0200

    Create Rectangle.js

---
+++ importable/Rectangle.js
@@ -0,0 +1,11 @@
+Rectangle = class {
+    constructor(x = 0, y = 0, width= 10, height=10) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+    draw(ctx){
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
