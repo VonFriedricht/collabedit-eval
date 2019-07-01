eef371ca71af512287d43fe18c41bc4123c58047
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-01 19:26:19 +0200

    renamed msg to messageElement

--- src/chat.js
+++ src/chat.js
@@ -1,16 +1,15 @@
 chat = {
   log: message => {
-    var chatDiv = document.getElementById('message_div')
+    var chatBox = document.getElementById('message_div')
 
-    var msg = document.createElement('pre')
-    msg.className = 'chtmsg'
-    msg.style.fontFamily = 'Consolas'
-    msg.style.fontSize = '1em'
+    var messageElement = document.createElement('pre')
+    messageElement.className = 'chtmsg'
+    messageElement.style.fontFamily = 'Consolas'
     messageElement.style.fontSize = '0.8em'
     message = JSON.stringify(message, null, 2)
-    msg.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${message}</span>`
-    chatDiv.appendChild(msg)
+    messageElement.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${message}</span>`
 
-    chatDiv.scrollTop = chatDiv.scrollHeight
+    chatBox.appendChild(messageElement)
+    chatBox.scrollTop = chatDiv.scrollHeight
   }
 }
