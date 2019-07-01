c3400f1c4bc57f6c131720a061bc128f70adbf90
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-01 19:42:28 +0200

    Create variableWatch.js

---
+++ src/variableWatch.js
@@ -0,0 +1,14 @@
+watch = content => {
+  let watch = document.getElementById('watch')
+  if (!watch) {
+    watch = document.createElement('pre')
+    watch.id = 'watch'
+    watch.style.border = '1px dotted gray'
+    watch.style.position = 'absolute'
+    watch.style.right = '5.5em'
+    watch.style.bottom = '9em'
+    document.getElementById('main').prepend(watch)
+  }
+  watch.innerHTML = JSON.stringify(content, null, 2)
+  return canvas
+}
