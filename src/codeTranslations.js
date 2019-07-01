7f7f42d9ab8e4c496953aa55d4817a905726dee0
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-01 19:36:06 +0200

    changed translationlist to array

--- src/codeTranslations.js
+++ src/codeTranslations.js
@@ -1,7 +1,26 @@
-translation = code =>
-  code
-    .replace(/for ([a-zA-Z]*?) in range\(([^\s]*?)\)/g, 'for(let $1 = 0; $1 < $2; $1++)')
-    .replace(/for ([a-zA-Z]*?) in range\(([^\s]*?),([^\s]*?)\)/g, 'for(let $1 = $2; $1 <= $3; $1++)')
-    .replace(/([^\s]*?).clean\(\)/g, `if($1.canvas){$1.clearRect(0,0,$1.canvas.width,$1.canvas.height)}`)
-    .replace(/#import ([^\s]*?)\n/g, 'if(typeof $1 == "undefined"){require("importable/$1.js").then(e=>$1=e)}\n')
-    .replace(/wiederhole ([^\s]*?) mal/g, 'for(let _wiederholung = 0; _wiederholung < $1; _wiederholung++)')
+var translations = [
+  [
+    /for ([a-zA-Z]*?) in range\(([^\s]*?)\)/,
+    'for(let $1 = 0; $1 < $2; $1++)'
+  ],
+  [
+    /for ([a-zA-Z]*?) in range\(([^\s]*?),([^\s]*?)\)/, 
+    'for(let $1 = $2; $1 <= $3; $1++)'
+  ],
+  [
+    /([^\s]*?).clean\(\)/, 
+    'if($1.canvas){$1.clearRect(0,0,$1.canvas.width,$1.canvas.height)}'
+  ],
+  [
+    /#import ([^\s]*?)\n/, 
+    'if(typeof $1 == "undefined"){require("importable/$1.js").then(e=>$1=e)}\n'
+  ],
+  [
+    /wiederhole ([^\s]*?) mal/, 
+    'for(let _wiederholung = 0; _wiederholung < $1; _wiederholung++)'
+  ]
+]
+translation = code => {
+  translations.forEach(t => (code = code.replace(new RegExp(t[0], 'g'), t[1])))
+  return code
+}
