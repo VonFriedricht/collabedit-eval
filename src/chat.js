a7c94fbdb963d7f163daf67df25f030f76f28e2e
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-01 19:16:39 +0200

    changing chat.log to pre container

--- src/chat.js
+++ src/chat.js
@@ -1,16 +1,15 @@
 chat = {
   log: message => {
     var chatDiv = document.getElementById('message_div')
-    var msg = document.createElement('div')
 
+    var msg = document.createElement('pre')
     msg.className = 'chtmsg'
-    if (typeof message != 'string') {
-      msg.style.fontFamily = "Consolas"
-      message = JSON.stringify(message, null, 2)
-    }
+    msg.style.fontFamily = 'Consolas'
+    msg.style.fontSize = '1em'
+    message = JSON.stringify(message, null, 2)
     msg.innerHTML = `<span class="nickspan">log: </span><span class="normalchat">${message}</span>`
-
     chatDiv.appendChild(msg)
+
     chatDiv.scrollTop = chatDiv.scrollHeight
   }
 }
