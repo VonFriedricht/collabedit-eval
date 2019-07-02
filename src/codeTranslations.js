134bca736866bf8f48fa790ec94e159177fc7bf3
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2019-07-02 13:44:27 +0200

    made include an alias of import

--- src/codeTranslations.js
+++ src/codeTranslations.js
@@ -3,6 +3,7 @@ var translations = [
   [/for ([a-zA-Z]*?) in range\(([^\s]*?),([^\s]*?)\)/, 'for(let $1 = $2; $1 <= $3; $1++)'],
   [/([^\s]*?).clean\(\)/, 'if($1.canvas){$1.clearRect(0,0,$1.canvas.width,$1.canvas.height)}'],
   [/#import ([^\s]*?)\n/, 'if(typeof $1 == "undefined"){require("importable/$1.js").then(e=>$1=e)}\n'],
+  [/#include ([^\s]*?)\n/, 'if(typeof $1 == "undefined"){require("importable/$1.js").then(e=>$1=e)}\n'],
   [/wiederhole ([^\s]*?) mal/, 'for(let _wiederholung = 0; _wiederholung < $1; _wiederholung++)']
 ]
 translation = code => {
